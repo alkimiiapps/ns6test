dc7c8b19816da397b2d191e8861f3ad7d84bbe5a
johnmcauley <john@alkimii.com>
2021-03-15 16:50:11 +0100

    Add init files

---
+++ app/components/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <Page>
+    <ActionBar title="Welcome to NativeScript-Vue!"/>
+    <GridLayout columns="*" rows="*, 50">
+      <NewComponent row="0" col="0" src="https://picsum.photos/200" stretch="aspectFit"/>
+      <GridLayout row="1" columns="*, *">
+        <Button col="0" text="Go Back" @tap="navigateBack()"/>
+        <Button col="1" text="Next Page" @tap="nextPage()"/>
+      </GridLayout>
+    </GridLayout>
+  </Page>
+</template>
+
+<script >
+  // Components
+  import ListItem from './ListItem'
+  import Main from './Home'
+  import NewComponent from './NewComponent'
+  import {GC} from 'utils/utils'
+
+  export default {
+    components: {
+      ListItem,
+      NewComponent
+    },
+    data() {
+      return {
+        msg: 'Hello World!',
+        items: new Array(1045),
+        indexkk: 0
+      }
+    },
+    
+    methods: {
+      nextPage() {
+        
+        this.$navigateTo(Main, {
+          //clearHistory: true
+        })
+      },
+      navigateBack() {
+        console.log(this.$refs.myphoto)
+        this.$refs.myphoto.nativeView.destroy()
+        
+        this.$navigateBack()
+        GC()
+      }
+    },
+    beforeDestroy() {
+        console.log('beforeDestroy called')
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
---
+++ app/components/NewComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <Image ref="player" v-on="listeners" v-bind="attrs" />
+</template>
+<script>
+// This wraps the nativescript-videoplayer in a component that auto destroys, so you don't have to
+// https://github.com/nstudio/nativescript-videoplayer
+// using techniques described in https://zendev.com/2018/05/31/transparent-wrapper-components-in-vue.html
+export default {
+  computed: {
+    listeners() {
+      const { ...listeners } = this.$listeners;
+      return listeners;
+    },
+    attrs() {
+      const { ...attrs } = this.$attrs;
+      return attrs;
+    }
+  },
+  beforeDestroy() {
+    this.$refs.player.nativeView.destroy();
+  }
+};
+</script>
